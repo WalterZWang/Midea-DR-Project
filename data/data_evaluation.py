b3a4f1ada1ac8ef71079ed9c155bca4d456d540f
Wang Zhenyu <wwwzy127@163.com>
2022-12-07 17:09:25 +0800

    data imputation for data format from 'get_influxdb.py'

--- data/data_evaluation.py
+++ data/data_evaluation.py
@@ -5,8 +5,8 @@
 @Author  :   Zhenyu Wang 
 '''
 '''
-This module is used to evaluate missing rate of data.
-The data format comes from "get_influxDB.py" and "data_processing.py"
+This module is used to evaluate data by missing rate.
+The data format comes from "get_influxDB.py".
 '''
 
 import matplotlib.pyplot as plt
@@ -17,7 +17,6 @@ import pandas as pd
 import sys
 import pickle
 import copy
-import data_processing as dp
 import get_influxDB
 
 plt.style.use('seaborn-deep')
@@ -25,15 +24,17 @@ mpl.rcParams['axes.unicode_minus'] = False
 mpl.rcParams['font.size'] = 8
 
 
-def cal_missing_rate1(data, time_window, eval, para):
+def cal_missing_rate(data, time_window, eval, param):
     '''
+    Calculates the missing rate of the specified variable in the data
+
     data: data format comes from "get_influxDB.py".
     time_window: specifies the size of the time window, the minimum is 1(day).
-    eval: specifies the information to be evaluated.
-    para: other parameters, like time and rsp_num per hour.
+    eval: specifies the variable to be evaluated.
+    param: other parameters, like time and rsp_num per hour.
     '''
 
-    time = []
+    date = []
     missing_rate = copy.deepcopy(eval)
     for key in missing_rate.keys():
         if key == 'vrf_id': continue
@@ -55,162 +56,175 @@ def cal_missing_rate1(data, time_window, eval, para):
         }
     '''
     
-    for i in range(0, (para['end_time']-para['start_time']).days//time_window):
-        time1 = para['start_time'] + pd.to_timedelta(time_window*i, unit='D')
-        time2 = para['start_time'] + pd.to_timedelta(time_window*(i+1), unit='D')
+    for i in range(0, (param['end_time']-param['start_time']).days//time_window):
+        time1 = param['start_time'] + pd.to_timedelta(time_window*i, unit='D')
+        time2 = param['start_time'] + pd.to_timedelta(time_window*(i+1), unit='D')
         # print(time1, time2)
-        time.append(time1)
+        date.append(time1)
 
-        if 'blg_devSn_id' in eval.keys():
-            for str in eval['blg_devSn_id']:
-                if str == eval['vrf_id']:
+        for key in eval.keys():
+            if key == 'idr_data':        
+                j = 0
+                while 'idu_'+f'{j}' in data.VRF_rsp[eval['vrf_id']]:
+                    for str in eval['idr_data']:
+                        if i == 0: missing_rate['idr_data'][str].append([]) 
+                        missing_rate['idr_data'][str][j].append\
+                            (1 - data.VRF_rsp[eval['vrf_id']]['idu_'+f'{j}'].loc[time1:time2][str].count() / (param['rsp_num']*time_window*24))
+                    j = j+1
+
+            if key == 'odr_data':
+                for str in eval['odr_data']:
+                    missing_rate['odr_data'][str].append\
+                        (1 - data.VRF_rsp[eval['vrf_id']]['odu_129'].loc[time1:time2][str].count() / (param['rsp_num']*time_window*24))
+
+            if key == 'sys_data':
+                for str in eval['sys_data']:
+                    missing_rate['sys_data'][str].append\
+                        (1 - data.VRF_rsp[eval['vrf_id']]['sys_'+eval['vrf_id'].split('_')[1]].loc[time1:time2][str].count() / (param['rsp_num']*time_window*24))
+
+            if key == 'weather_data':
+                for str in eval['weather_data']:
+                    missing_rate['weather_data'][str].append\
+                        (1 - data.weather_rsp.loc[time1:time2][str].count() / (param['rsp_num']*time_window*24))
+
+            if key == 'blg_devSn_id':
+                for str in eval['blg_devSn_id']:
+                    if str == eval['vrf_id']:
+                        missing_rate['blg_devSn_id'][str].append\
+                            (1 - data.VRF_rsp[eval['vrf_id']]['meter'].loc[time1:time2]['E'].count() / (param['rsp_num']*time_window*24))
+                        continue
                     missing_rate['blg_devSn_id'][str].append\
-                        (1 - data.VRF_rsp[eval['vrf_id']]['meter'].loc[time1:time2]['E'].count() / (para['rsp_num']*time_window*24))
-                    continue
-                missing_rate['blg_devSn_id'][str].append\
-                    (1 - data.blg_meter_rsp[str].loc[time1:time2]['E'].count() / (para['rsp_num']*time_window*24))
-                
-        if 'idr_data' in eval.keys():
-            j = 0
-            while 'idu_'+f'{j}' in data.VRF_rsp[eval['vrf_id']]:
-                for str in eval['idr_data']:
-                    missing_rate['idr_data'][str].append([]) 
-                    missing_rate['idr_data'][str][j].append\
-                        (1 - data.VRF_rsp[eval['vrf_id']]['idu_'+f'{j}'].loc[time1:time2][str].count() / (para['rsp_num']*time_window*24))
-                j = j+1
+                        (1 - data.blg_meter_rsp[str].loc[time1:time2]['E'].count() / (param['rsp_num']*time_window*24))
 
-        if 'odr_data' in eval.keys():
-            for str in eval['odr_data']:
-                missing_rate['odr_data'][str].append\
-                    (1 - data.VRF_rsp[eval['vrf_id']]['odu_129'].loc[time1:time2][str].count() / (para['rsp_num']*time_window*24))
+    return missing_rate, date
 
-        if 'sys_data' in eval.keys():
-            for str in eval['sys_data']:
-                missing_rate['sys_data'][str].append\
-                    (1 - data.VRF_rsp[eval['vrf_id']]['sys_'+eval['vrf_id'].split('_')[1]].loc[time1:time2][str].count() / (para['rsp_num']*time_window*24))
+def plot_missing_rate(missing_rate, date, time_window, mark):
+    '''
+    Plot missing rate over time
+    '''
 
-        if 'weather_data' in eval.keys():
-            for str in eval['weather_data']:
-                missing_rate['weather_data'][str].append\
-                    (1 - data.weather_rsp.loc[time1:time2][str].count() / (para['rsp_num']*time_window*24))
+    for i in range(0, len(missing_rate['idr_data']['roomTemp'])):
+        plt.plot(date, missing_rate['idr_data']['roomTemp'][i], label=f'idu_{i}_'+'roomTemp', marker='D')
+    for i in range(0, len(missing_rate['idr_data']['onOff'])):
+        plt.plot(date, missing_rate['idr_data']['onOff'][i], label=f'idu_{i}_'+'onOff', marker='p')
 
-    # plot
-    i = 0
-    while 'idu_'+f'{i}' in data.VRF_rsp[eval['vrf_id']]:
-        plt.plot(time, missing_rate['idr_data']['roomTemp'][i], label=f'idu_{i}_'+'roomTemp', marker='D')
-        i = i+1
-    i = 0
-    while 'idu_'+f'{i}' in data.VRF_rsp[eval['vrf_id']]:
-        plt.plot(time, missing_rate['idr_data']['onOff'][i], label=f'idu_{i}_'+'onOff', marker='p')
-        i = i+1
+    plt.plot(date, missing_rate['blg_devSn_id']['mDev_EMeter_F2_Backup'], label='E_lightplug', marker='x')
+    plt.plot(date, missing_rate['blg_devSn_id'][missing_rate['vrf_id']], label='E_vrf', marker='x')
 
-    plt.plot(time, missing_rate['blg_devSn_id']['mDev_EMeter_F2_Backup'], label='E_lightplug', marker='x')
-    plt.plot(time, missing_rate['blg_devSn_id'][eval['vrf_id']], label='E_vrf', marker='x')
+    plt.plot(date, missing_rate['odr_data']['t4Temp'], label='odu_t4Temp', marker='o')
+    plt.plot(date, missing_rate['odr_data']['powerNeed'], label='odu_powerNeed', marker='o')
+    plt.plot(date, missing_rate['sys_data']['systemQc'], label='sys_systemQc', marker='o')
 
-    plt.plot(time, missing_rate['odr_data']['t4Temp'], label='odu_t4Temp', marker='o')
-    plt.plot(time, missing_rate['odr_data']['powerNeed'], label='odu_powerNeed', marker='o')
-    plt.plot(time, missing_rate['sys_data']['systemQc'], label='sys_systemQc', marker='o')
+    plt.plot(date, missing_rate['weather_data']['e3'], label='weather_e3', marker='*')
+    plt.plot(date, missing_rate['weather_data']['e11'], label='weather_e11', marker='*')
 
-    plt.plot(time, missing_rate['weather_data']['e3'], label='weather_e3', marker='*')
-    plt.plot(time, missing_rate['weather_data']['e11'], label='weather_e11', marker='*')
-
-    plt.title(eval['vrf_id']+f' Missing Rate (Per {time_window} day)')
+    plt.title(missing_rate['vrf_id']+f' Missing Rate (Per {time_window} day) '+mark)
     plt.xlabel("Date")
     plt.ylabel("Missing rate")
     plt.legend(loc = 'upper left')
 
     plt.gcf().set_size_inches(16, 8)
-    plt.savefig(f'./results/data_evaluation/'+eval['vrf_id']+f' Missing Rate (Per {time_window} day).png', dpi=300, bbox_inches='tight', pad_inches=0.1)
+    plt.savefig(f'./results/data_evaluation/'+missing_rate['vrf_id']+f' Missing Rate (Per {time_window} day) '+mark+'.png', dpi=300, bbox_inches='tight', pad_inches=0.1)
     # plt.show()
     
     plt.close()
     # plt.clf()
-    
+
+    print(missing_rate['vrf_id'] + f' Missing Rate (Per {time_window} day) ' + mark)
     return
 
-def cal_missing_rate2(data, time_window, eval, para):
+def remove_outlier_df(data_frame):
+    '''remove outliers in a data_frame'''
+
+    # 删除小于0的值
+    data_frame[data_frame < 0]  = np.nan
+    # 利用箱线图去除离群值, 会把不是异常值的判定为异常值
+    Dcos = data_frame.quantile(0.95) - data_frame.quantile(0.05)
+    L = data_frame.quantile(0.05) - 1.5 * Dcos
+    U = data_frame.quantile(0.95) + 1.5 * Dcos
+    data_frame[data_frame < L] = np.nan
+    data_frame[data_frame > U] = np.nan
+
+    return data_frame
+
+def remove_outliers(data, eval):
     '''
-    data: data format comes "data_processing.py".
+    Remove outliers
+
+    data: data format comes from "get_influxDB.py".
     '''
 
-    time = []
-    missing_rate = copy.deepcopy(eval)
-    for key in missing_rate.keys():
-        if key == 'vrf_id': continue
-        missing_rate[key] = dict(zip(missing_rate[key], [[] for i in range(len(missing_rate[key]))]))
+    data_process = copy.deepcopy(data)
+
+    data_process.weather_rsp = remove_outlier_df(data_process.weather_rsp)
+    for key, value in data_process.VRF_rsp[eval['vrf_id']].items():
+        data_process.VRF_rsp[eval['vrf_id']][key] = remove_outlier_df(value)
+    for key, value in data_process.blg_meter_rsp.items():
+        data_process.blg_meter_rsp[key] = remove_outlier_df(value)
+
+    return data_process
+
+def linear_imputation_df(data_frame, imputation_time_delta, sampling_time_delta):
+    '''
+    Linear interpolation of a data_frame
+
+    data_frame: ata that belongs to the "data_frame" format.
+    imputation_time_delta: data loss for more than consecutive imputation_time_delta will not be interpolated.
+    sampling_time_delta: sampling interval (15min).
+    '''
+
+    for col in data_frame.columns:
+        series = data_frame[col]
+
+        # 获取空值索引
+        series_nan = series[series.isna()]
+        series_nan_index = series_nan.index
+        if series_nan_index.empty: continue
+        
+        # 获取小于imputation_time_delta的空值索引
+        col_imputation_list = []
+        start = series_nan_index[0]
+        temp = series_nan_index[0]
+        end = series_nan_index[0]
+        for i in range(len(series_nan_index)-1):
+            end = series_nan_index[i+1]
+            if (end-temp) == sampling_time_delta:
+                temp = end
+            else:
+                if (temp - start) <= imputation_time_delta and (start!=series.head(1).index and end!=series.tail(1).index):
+                    col_imputation_list.append([start, temp])
+                start = end
+                temp = end
+        if (temp - start) <= imputation_time_delta and (start!=series.head(1).index and end!=series.tail(1).index):
+            col_imputation_list.append([start, temp])
+
+        # 对满足imputation_time_delta的空值进行线性插值
+        for imputation in col_imputation_list:
+            data_frame[col].loc[imputation[0]-sampling_time_delta: imputation[1]+sampling_time_delta] = \
+                series.loc[imputation[0]-sampling_time_delta: imputation[1]+sampling_time_delta].interpolate(method='polynomial', order=1)
+        
+    return data_frame
+
+def polynomial_imputation(data, eval, param):
+    '''
+    Polynomial interpolation (currently only linear interpolation)
+
+    data: data format comes from "get_influxDB.py".
+    '''
+
+    data_new = copy.deepcopy(data)
+
+    data_new.weather_rsp = \
+        linear_imputation_df(data_new.weather_rsp, param['imputation_time_delta'], param['rsp_time'])
+    for key, value in data_new.VRF_rsp[eval['vrf_id']].items():
+        data_new.VRF_rsp[eval['vrf_id']][key] = \
+            linear_imputation_df(value, param['imputation_time_delta'], param['rsp_time'])
+    for key, value in data_new.blg_meter_rsp.items():
+        data_new.blg_meter_rsp[key] = \
+            linear_imputation_df(value, param['imputation_time_delta'], param['rsp_time'])
     
-    for i in range(0, (para['end_time']-para['start_time']).days//time_window):
-        time1 = para['start_time'] + pd.to_timedelta(time_window*i, unit='D')
-        time2 = para['start_time'] + pd.to_timedelta(time_window*(i+1), unit='D')
-        # print(time1, time2)
-        time.append(time1)
+    return data_new
 
-        if 'blg_devSn_id' in eval.keys():
-            for str in eval['blg_devSn_id']:
-                if str == eval['vrf_id']:
-                    missing_rate['blg_devSn_id'][str].append\
-                        (1 - data.VRF_data_pro[eval['vrf_id']]['meter'].loc[time1:time2]['E'].count() / (para['rsp_num']*time_window*24))
-                    continue
-                missing_rate['blg_devSn_id'][str].append\
-                    (1 - data.blg_meter_pro[str].loc[time1:time2]['E'].count() / (para['rsp_num']*time_window*24))
-                
-        if 'idr_data' in eval.keys():
-            j = 0
-            while 'idu_'+f'{j}' in data.VRF_data_pro[eval['vrf_id']]:
-                for str in eval['idr_data']:
-                    missing_rate['idr_data'][str].append([]) 
-                    missing_rate['idr_data'][str][j].append\
-                        (1 - data.VRF_data_pro[eval['vrf_id']]['idu_'+f'{j}'].loc[time1:time2][str].count() / (para['rsp_num']*time_window*24))
-                j = j+1
-
-        if 'odr_data' in eval.keys():
-            for str in eval['odr_data']:
-                missing_rate['odr_data'][str].append\
-                    (1 - data.VRF_data_pro[eval['vrf_id']]['odu_129'].loc[time1:time2][str].count() / (para['rsp_num']*time_window*24))
-
-        if 'sys_data' in eval.keys():
-            for str in eval['sys_data']:
-                missing_rate['sys_data'][str].append\
-                    (1 - data.VRF_data_pro[eval['vrf_id']]['sys_'+eval['vrf_id'].split('_')[1]].loc[time1:time2][str].count() / (para['rsp_num']*time_window*24))
-
-        if 'weather_data' in eval.keys():
-            for str in eval['weather_data']:
-                missing_rate['weather_data'][str].append\
-                    (1 - data.weather_pro.loc[time1:time2][str].count() / (para['rsp_num']*time_window*24))
-
-    # plot
-    i = 0
-    while 'idu_'+f'{i}' in data.VRF_data_pro[eval['vrf_id']]:
-        plt.plot(time, missing_rate['idr_data']['roomTemp'][i], label=f'idu_{i}_'+'roomTemp', marker='D')
-        i = i+1
-    i = 0
-    while 'idu_'+f'{i}' in data.VRF_data_pro[eval['vrf_id']]:
-        plt.plot(time, missing_rate['idr_data']['onOff'][i], label=f'idu_{i}_'+'onOff', marker='p')
-        i = i+1
-
-    plt.plot(time, missing_rate['blg_devSn_id']['mDev_EMeter_F2_Backup'], label='E_lightplug', marker='x')
-    plt.plot(time, missing_rate['blg_devSn_id'][eval['vrf_id']], label='E_vrf', marker='x')
-
-    plt.plot(time, missing_rate['odr_data']['t4Temp'], label='odu_t4Temp', marker='o')
-    plt.plot(time, missing_rate['odr_data']['powerNeed'], label='odu_powerNeed', marker='o')
-    plt.plot(time, missing_rate['sys_data']['systemQc'], label='sys_systemQc', marker='o')
-
-    plt.plot(time, missing_rate['weather_data']['e3'], label='weather_e3', marker='*')
-    plt.plot(time, missing_rate['weather_data']['e11'], label='weather_e11', marker='*')
-
-    plt.title(eval['vrf_id']+f' Missing Rate (Per {time_window} day)(process)')
-    plt.xlabel("Date")
-    plt.ylabel("Missing rate")
-    plt.legend(loc = 'upper left')
-
-    plt.gcf().set_size_inches(16, 8)
-    plt.savefig(f'./results/data_evaluation/'+eval['vrf_id']+f' Missing Rate (Per {time_window} day)(process).png', dpi=300, bbox_inches='tight', pad_inches=0.1)
-    # plt.show()
-    
-    plt.close()
-    # plt.clf()
-    
-    return
 
 
 # # get data from ''get_infuxDB.py''
@@ -224,7 +238,7 @@ def cal_missing_rate2(data, time_window, eval, para):
 # print('data saved')
 
 # or load local data
-file = open(r'C:\Users\Wang\Desktop\data.pkl','rb')  
+file = open(r'C:\Users\ADMIN\Desktop\data.pkl','rb')  
 data = pickle.load(file)  
 file.close()  
 print('local data loaded')
@@ -237,24 +251,26 @@ eval = {'vrf_id': 'VRF_1K0V',
         'blg_devSn_id': ['mDev_EMeter_F2_Backup', 'VRF_1K0V'],
         'weather_data': ['e3', 'e11']
 }
-para = {
+param = {
     'start_time': pd.to_datetime('2022-05-01 00:00:00+08:00'),
     'end_time': pd.to_datetime('2022-11-23 00:00:00+08:00'),
-    'rsp_num': 4  # rsp = '15T' --> rsp_num = 4 times per hour
+    'rsp_num': 4,  # rsp = '15T' --> rsp_num = 4 times per hour
+    'rsp_time': pd.Timedelta('15 minutes'),
+    'imputation_time_delta': pd.Timedelta('6 hours')
 }
+
 
 # evaluate missing rate of data and save results
 for time_window in range(1, 8, 2):
-    cal_missing_rate1(data, time_window, eval, para)
-    print(f'Missing Rate (Per {time_window} day)')
-data_process = dp.DataProcess(data)
-for time_window in range(1, 8, 2):
-    cal_missing_rate2(data_process, time_window, eval, para)
-    print(f'Missing Rate (Per {time_window} day)(process)')
+    missing_rate, date = cal_missing_rate(data, time_window, eval, param)
+    plot_missing_rate(missing_rate, date, time_window, mark='origin')
 
-'''
-data_process与data内部属性命名不一致, 故存在冗余的两个函数cal_missing_rate1与cal_missing_rate2
-dp.DataProcess(data)内是对data的浅拷贝, process后data的子对象也被更改, 所以不能修改后在一个循环内一起绘图
-dp.DataProcess中修改了使用的utility.py的函数: util.outlier_meter --> util.outlier
-util.outlier修改了箱线图的分位点参数quantile
-'''
+data_process = remove_outliers(data, eval)
+for time_window in range(1, 8, 2):
+    missing_rate, date = cal_missing_rate(data_process, time_window, eval, param)
+    plot_missing_rate(missing_rate, date, time_window, mark='remove_outliers')
+
+data_new = polynomial_imputation(data_process, eval, param)
+for time_window in range(1, 8, 2):
+    missing_rate, date = cal_missing_rate(data_new, time_window, eval, param)
+    plot_missing_rate(missing_rate, date, time_window, mark='polynomial_imputation')
